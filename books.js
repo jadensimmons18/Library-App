674b89e6f1da2cf034c7ff2c884d0d83aa73f729
Jaden Simmons <jadensimmons1803@gmail.com>
2026-04-04 18:25:20 -0400

    created frontend search screen and completed backend search logic

--- books.js
+++ books.js
@@ -2,7 +2,7 @@
 
 
 
-
+const Library = [];
 
 
 
@@ -12,6 +12,7 @@ class Book {
         this.author = author;
         this.pages = pages;
         this.read = read;
+        this.id = crypto.randomUUID();
     }
 
     info() {
@@ -20,5 +21,37 @@ class Book {
     }
 }
 
+function addBookToLibrary(book){
+    Library.push(book);
+}
+
+function search(input) {
+
+    const results = [];
+
+    const lowerInput = input.toLowerCase();
+
+    for (let i = 0; i < Library.length; i++){
+        if (Library[i].title.toLowerCase().includes(lowerInput) || Library[i].author.toLowerCase().includes(lowerInput)) {
+            results.push(Library[i]);
+        }
+    }
+    return results;
+}
+
+
 theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, true);
-console.log(theHobbit.info());
+addBookToLibrary(theHobbit);
+harryPotter = new Book("Harry Potter", "i dont remember", 365, false);
+addBookToLibrary(harryPotter);
+newYear = new Book("New year", "J.R.R.", 295, true);
+addBookToLibrary(newYear);
+hob = new Book("The Hob", "J.R.R. Tolkien", 295, true);
+addBookToLibrary(hob);
+
+
+console.log(search("hob"));
+console.log(search("the"));
+console.log(search("j.r.r."))
+
+
